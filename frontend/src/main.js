51c2bb8fea0b7738c3e0c17c9d0cf70ce570739e
Anish Kumar <anishk795@gmail.com>
2018-12-09 23:07:34 +0530

    search bar setup done

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,20 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+/* eslint-disable */
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.config.productionTip = false
+import Vue from 'vue';
+import App from './App';
+import BootStrapVue from 'bootstrap-vue';
+import axios from 'axios';
+import router from './router';
+
+Vue.config.productionTip = false;
+Vue.use(BootStrapVue, axios);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: '<App/>',
+});
